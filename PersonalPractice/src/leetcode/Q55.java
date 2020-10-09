480f7ec6fc617e698993caf89afe275b6b396b7b
Ltyro <17816874829@163.com>
2020-10-09 10:26:48 +0800

    旋转矩阵2

--- PersonalPractice/src/leetcode/Q55.java
+++ PersonalPractice/src/leetcode/Q55.java
@@ -1,23 +1,11 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ * 给定一个正整数 n，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
  * 
- * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 示例:
  * 
- * 判断你是否能够到达最后一个位置。
- * 
- * 示例 1:
- * 
- * 输入: [2,3,1,1,4] 输出: true 解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3步到达最后一个位置。
- * 示例 2:
- * 
- * 输入: [3,2,1,0,4] 输出: false 解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0
- * ，所以你永远不可能到达最后一个位置。
- * 
+ * 输入: 3 输出: [ [ 1, 2, 3 ], [ 8, 9, 4 ], [ 7, 6, 5 ] ]
  * 
  * 
  * @author Zaoji_Lai
@@ -27,22 +15,25 @@ import java.util.List;
 public class Q55 {
 
 	public static void main(String[] args) {
-//		int[] nums = { 2, 3, 1, 1, 4 };
-		int[] nums = {3,2,1,0,4};
-		boolean l = canJump(nums);
+		int[][] l = generateMatrix(3);
 		System.out.println(l);
 	}
 
-	public static boolean canJump(int[] nums) {
-		int k = 0;
-		for (int i = 0; i < nums.length; i++) {
-			if (i > k) {
-				return false;
-			}
-			k = Math.max(k, i + nums[i]);
+	public static int[][] generateMatrix(int n) {
+		int a[][] = new int[n][n];
+		int max = n * n, num = 1;
+		int l = 0, r = n - 1, t = 0, b = n - 1;
+		while (num <= max) {
+			for (int i = l; i <= r; i++) a[t][i] = num++;
+			t++;
+			for (int i = t; i <= b; i++) a[i][r] = num++;
+			r--;
+			for (int i = r; i >= l; i--) a[b][i] = num++;
+			b--;
+			for (int i = b; i >= t; i--) a[i][l] = num++;
+			l++;
 		}
-		return true;
-
+		return a;
 	}
 
 }
